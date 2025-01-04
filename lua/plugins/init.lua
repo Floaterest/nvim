cb15bf9145819f17a13a1ef7f83b455566901e88
floaterest <floaterest@users.noreply.github.com>
2025-01-04 13:14:46 -0500

    init.lua lazy-lock.json

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,6 +25,7 @@ local noice = require('lazy').setup({
     -- { 'floaterest/typst-preview.nvim', ft = 'typst', opts = config.typst },
     { 'chomosuke/typst-preview.nvim', ft = 'typst', opts = config.typst },
     { "tiagovla/scope.nvim", opts = {} },
+    { "willothy/flatten.nvim", config = true, lazy = false, priority = 1001},
     { 'L3MON4D3/LuaSnip', config = req('plugins.luasnip') },
     { 'Shatur/neovim-session-manager', config = config.session },
     { 'akinsho/bufferline.nvim', opts = config.bufferline },
